170ce9c4ba518b6558cfe281d519659218f92928
dekoms <dekoms@naver.com>
2024-09-21 12:58:55 +0900

    feat: NavigationBar 컴포넌트 구현
    navItems.ts로 텍스트 props로 관리
    
    LR-29 #time 30m

---
+++ src/components/NavigationBar/navItems.ts
@@ -0,0 +1,12 @@
+// navItems.ts
+
+// eslint-disable-next-line import/prefer-default-export
+export const navItems: string[] = [
+  '회원가입',
+  '마이페이지',
+  '아이디 찾기',
+  '로그인',
+  '터미널혼잡도 조회',
+  '본선작업현황',
+  '반출입진행 조회',
+];
