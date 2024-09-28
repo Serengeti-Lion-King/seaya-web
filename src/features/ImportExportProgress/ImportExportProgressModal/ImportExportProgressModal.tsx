c4b1674da5a7e819fabd3b48f4497e789fe740ed
dekoms <dekoms@naver.com>
2024-09-28 18:48:49 +0900

    feat: 모달창 접근성 개선
    ESC로 모달창 닫기 및 모달 닫은 뒤 이전 포커스 복원
    추후 스크린 리더 고려한 role, aria 추가 예정
    
    LR-25 #time 1h

--- src/features/ImportExportProgress/ImportExportProgressModal/ImportExportProgressModal.tsx
+++ src/features/ImportExportProgress/ImportExportProgressModal/ImportExportProgressModal.tsx
@@ -11,21 +11,53 @@ const ImportExportProgressModal = ({
   onClose,
 }: ImportExportModalProps) => {
   const closeButtonRef = useRef<HTMLButtonElement>(null);
+  const previousActiveElement = useRef<Element | null>(null);
 
   useEffect(() => {
-    // 모달이 열리면 첫 번째 버튼에 포커스를 이동
-    if (isOpen && closeButtonRef.current) {
-      closeButtonRef.current.focus();
+    if (isOpen) {
+      // 모달이 열리면 이전 포커스 요소 저장
+      previousActiveElement.current = document.activeElement;
+      // 첫 번째 버튼에 포커스 이동
+      closeButtonRef.current?.focus();
+
+      const handleKeyDown = (event: KeyboardEvent) => {
+        if (event.key === 'Escape') {
+          onClose();
+        }
+      };
+
+      document.addEventListener('keydown', handleKeyDown);
+
+      // Cleanup: 키 이벤트 제거
+      return () => {
+        document.removeEventListener('keydown', handleKeyDown);
+        // 모달이 닫힐 때 이전 포커스 요소로 포커스 복원
+        if (previousActiveElement.current instanceof HTMLElement) {
+          previousActiveElement.current.focus();
+        }
+      };
     }
-  }, [isOpen]);
+
+    // `isOpen`이 `false`일 때는 아무 것도 하지 않음
+    return undefined;
+  }, [isOpen, onClose]);
 
   if (!isOpen) return null;
-
   return (
     <div className="modal-backdrop">
-      <div className="modal-content">
-        <p>아직 작업 중에 있습니다.</p>
-        <button type="button" className="modal-button" onClick={onClose}>
+      <div
+        className="modal-content"
+        role="dialog"
+        aria-modal="true"
+        aria-labelledby="modal-title"
+      >
+        <p id="modal-title">아직 작업 중에 있습니다.</p>
+        <button
+          type="button"
+          className="modal-button"
+          onClick={onClose}
+          ref={closeButtonRef}
+        >
           확인
         </button>
       </div>
