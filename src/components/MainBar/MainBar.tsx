4cbc0f0a59be708a8b17cebf7484070ddcfbdce8
yn_nub <129060841+soooheeee@users.noreply.github.com>
2024-09-28 16:59:05 +0900

    feat: MainBar 컴포넌트 생성 및 스타일 추가 MainBar 컴포넌트 생성
    LR-28 #time 30m

---
+++ src/components/MainBar/MainBar.tsx
@@ -0,0 +1,12 @@
+import './MainBar.scss';
+import SeayaIcon from '@assets/seayaIcon.svg?react';
+
+const MainBar = () => {
+  return (
+    <div className="logo">
+      <SeayaIcon />
+    </div>
+  );
+};
+
+export default MainBar;
