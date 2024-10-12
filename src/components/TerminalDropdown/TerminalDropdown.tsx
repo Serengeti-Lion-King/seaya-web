e155347865e9b21791c67b6b1600722c8cdd9c71
dekoms <dekoms@naver.com>
2024-10-13 04:18:20 +0900

    fix: PortDropdown OOCSS 적용
    class명 수정 및 Dropdown.scss 통합
    
    LR-18 #time 3h

--- src/components/TerminalDropdown/TerminalDropdown.tsx
+++ src/components/TerminalDropdown/TerminalDropdown.tsx
@@ -1,5 +1,6 @@
+import ArrowDownIcon from '@assets/arrow_drop_down.svg?react';
 import useDropdown from '@hooks/useDropdown';
-import './TerminalDropdown.scss';
+import '../../styles/Dropdown.scss';
 
 interface TerminalDropdownProps {
   options: string[];
@@ -11,32 +12,34 @@ const TerminalDropdown = ({ options, label }: TerminalDropdownProps) => {
     useDropdown(label);
 
   return (
-    <div className="terminal-dropdown">
-      <button
-        type="button"
-        className="terminal-dropdown__toggle"
-        onClick={toggleDropdown}
-      >
-        {selectedOption || label}
-        <span
-          className={`terminal-dropdown__arrow ${isOpen ? 'terminal-dropdown__arrow--up' : 'terminal-dropdown__arrow--down'}`}
-        />
-      </button>
-      {isOpen && (
-        <ul className="terminal-dropdown__menu">
-          {options.map(option => (
-            <li className="terminal-dropdown__option" key={option}>
-              <button
-                type="button"
-                onClick={() => handleOptionClick(option)}
-                className="terminal-dropdown__option-button"
-              >
-                {option}
-              </button>
-            </li>
-          ))}
-        </ul>
-      )}
+    <div className="dropdown-container">
+      <div className="dropdown">
+        <button
+          type="button"
+          className="dropdown-toggle"
+          onClick={toggleDropdown}
+        >
+          <span className="dropdown-text">{selectedOption || label}</span>
+          <ArrowDownIcon
+            className={`arrow-icon ${isOpen ? 'arrow-icon--up' : 'arrow-icon--down'}`}
+          />
+        </button>
+        {isOpen && (
+          <ul className="dropdown-menu">
+            {options.map(option => (
+              <li className="menu-option" key={option}>
+                <button
+                  className="option-button"
+                  type="button"
+                  onClick={() => handleOptionClick(option)}
+                >
+                  {option}
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 };
